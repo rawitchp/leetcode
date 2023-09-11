4a3f581def0f2a37cef1cba77040d2798e6800d2
Rawitch Payakkawan <rawitch13@gmail.com>
2023-09-11 20:38:08 +0700

    2790

--- Medium/2790. Maximum Number of Groups With Increasing Length.js
+++ Medium/2790. Maximum Number of Groups With Increasing Length.js
@@ -2,16 +2,15 @@
  * @param {number[]} groupSizes
  * @return {number[][]}
  */
-var groupThePeople = function (groupSizes) {
-  let ans = [];
-  let hashMap = {};
-  groupSizes.map((num, i) => {
-    console.log(hashMap[num]);
-    hashMap.hasOwnProperty(num) ? hashMap[num].push(i) : (hashMap[num] = [i]);
-    if (hashMap[num].length == num) {
-      ans.push(hashMap[num]);
-      hashMap[num] = [];
-    }
-  });
-  return ans;
+ var groupThePeople = function(groupSizes) {
+    let ans = []
+    let hashMap = {}
+    groupSizes.map((num,i)=>{
+        hashMap[num]? hashMap[num].push(i): hashMap[num] = [i]
+        if(hashMap[num].length == num){
+            ans.push(hashMap[num])
+            delete hashMap[num]
+        }
+    })
+    return ans
 };

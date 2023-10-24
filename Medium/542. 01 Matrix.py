18b406c138dd2fe51d97f26d45df201b7ab01e8f
Rawitch Payakkawan <rawitch13@gmail.com>
2023-10-24 22:45:34 +0700

    542

---
+++ Medium/542. 01 Matrix.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def updateMatrix(self, mat):
+        """
+        :type mat: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        Row, Col = len(mat), len(mat[0])
+        queue = []
+        directions = [[0, +1], [0, -1], [1, 0], [-1, 0]]
+        for i in range(Row):
+			for j in range(Col):
+				if mat[i][j] == 0:
+					queue.append((i, j)) 
+				else:
+					mat[i][j] = "*"
+        print(queue)
+        for r, c in queue:
+			for dr, dc in directions:
+				row = r + dr
+				col = c + dc
+				if 0 <= row < Row and 0 <= col < Col and mat[row][col] == "*":
+					mat[row][col] = mat[r][c] + 1
+					queue.append((row, col))
+        return mat

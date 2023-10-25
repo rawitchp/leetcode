3b53a43f3c695949d780da05735188b5cc1333c4
Rawitch Payakkawan <rawitch13@gmail.com>
2023-10-25 11:15:07 +0700

    208

---
+++ Medium/208. Implement Trie (Prefix Tree).py
@@ -0,0 +1,52 @@
+class Trie(object):
+
+    def __init__(self):
+        self.root = {}
+      
+        
+
+    def insert(self, word):
+        """
+        :type word: str
+        :rtype: None
+        """
+        node = self.root
+        for ch in word:
+            if ch not in node:
+                node[ch] = {}
+            node = node[ch]
+        node['end'] = {}
+
+    def search(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        node = self.root
+        for ch in word:
+            if ch not in node:
+                return False
+            node = node[ch]
+        return 'end' in node
+        
+
+    def startsWith(self, prefix):
+        """
+        :type prefix: str
+        :rtype: bool
+        """
+        node = self.root
+        for ch in prefix:
+            if ch not in node:
+                return False
+            node = node[ch]
+        return True
+        
+        
+
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)

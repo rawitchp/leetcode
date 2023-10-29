51245aaa197725fbd49330c4f54dd36d215e0296
Rawitch Payakkawan <rawitch13@gmail.com>
2023-10-29 15:26:58 +0700

    416

---
+++ Medium/416. Partition Equal Subset Sum.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        dp, s = set([0]), sum(nums)
+        if s&1:
+            return False
+        for num in nums:
+            for curr in range(s>>1, num-1, -1):
+                if curr not in dp and curr-num in dp:
+                    if curr == s>>1:
+                        return True
+                    dp.add(curr)
+        return False
